cf7e1171d85a1053bb86f35d5ca4d58da5a98f85
Vadim Bolshakov <vadim_bolshakov@epam.com>
2017-08-08 00:44:10 +0300

    temp

---
+++ ConsoleApp1/MyDecorator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyDecorator:MethodDecoratorInterfaces.MethodDecoratorAttribute
+    {
+        string Mn;
+        public override void Init(object instance, MethodBase method, object[] args)
+        {
+            Mn = method.Name;
+            base.Init(instance, method, args);
+        }
+
+        public override void OnEntry()
+        {
+            Console.WriteLine("Entering" + Mn);
+            base.OnEntry();
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            Console.WriteLine("Exiting " + Mn);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodDecoratorInterfaces;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Internal(6));
+        }
+
+        [MyDecorator]
+        private static int Internal(int i) {
+            return i + 5;
+        }
+    }
+}
